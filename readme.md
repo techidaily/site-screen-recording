85e2c256bb2bdb6b2a55319765d2ddb74364bbc8
lauer3912 <lauer3912@gmail.com>
2024-05-19 07:43:24 +0800

    init

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-# TechiDaily
+# Screen Recording Site
 
-Welcome to [https://android-transfer.techidaily.com](https://android-transfer.techidaily.com)
+Welcome to [https://screen-recording.techidaily.com](https://screen-recording.techidaily.com)
 
 ## Publish
 
--- submit.bing.js
+++ submit.bing.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 
-const indexNowKey = 'ec9c9516ea82433086d979e16e1960e0'; // 替换为你的API密钥
-const siteHostName = 'android-transfer.techidaily.com'; // 替换为你的网站URL
+const indexNowKey = 'c031638a6d0b4dad94014febd61b8788'; // 替换为你的API密钥
+const siteHostName = 'screen-recording.techidaily.com'; // 替换为你的网站URL
 const groupItemCount = 100; // 每组最多个URL
 
 
